da92fdaa20a926c9dc332b6fda0b54607347b2f4
Aricnos <palashchaudhary@ymail.com>
2025-05-05 14:43:08 +0530

    updated

---
+++ src/config.py
@@ -0,0 +1,3 @@
+BRAVE_PATH = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+CHROMEDRIVER_PATH = r"C:\Users\M S I\node_modules\chromedriver\lib\chromedriver\chromedriver.exe"
+PAGE_LOAD_WAIT = 5 # seconds
